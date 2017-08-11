ae8800a14afda4be6f01892259d76892027ce837
Celia <celia25winters@gmail.com>
2017-08-10 21:46:46 -0500

    Added url to homepage Added view for homepage

--- wishlist/models.py
+++ wishlist/models.py
@@ -3,7 +3,12 @@ from django.contrib.auth.models import User
 import math
 
 
-# Create your models here.
+class CamSite(models.Model):
+    site_name = models.CharField(max_length=30)
+    username = models.CharField(max_length=30)
+    link = models.CharField(max_length=400)
+
+
 class Model(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     cam_sites = models.ManyToManyField(CamSite, null=True)
@@ -12,28 +17,14 @@ class Model(models.Model):
     city = models.CharField(max_length=30, blank=True)
     country = models.CharField(max_length=30)
 
+class Vendor(models.Model):
+    drop_shipping_price = models.DecimalField(decimal_places=2, max_digits=6)
 
-class CamSite(models.Model):
-    site_name = models.CharField(max_length=30)
-    username = models.CharField(max_length=30)
-    link = models.CharField(max_length=400)
-
-
-class WishList(models.Model):
-    model = models.ForeignKey(models)
-    title = models.CharField(max_length=40)
-    description = models.TextField(blank=True)
-
-
-class WishListLines(models):
-    product = models.ForeignKey(Product)
-    wish_list = models.ForeignKey(WishList)
-    purchased = models.BooleanField(default=False)
 
 
 class Product(models.Model):
-    cost = models.DecimalField(decimal_places=2, max_length=12)
-    price = models.DecimalField(decimal_places=2, max_length=12)
+    cost = models.DecimalField(decimal_places=2, max_digits=12)
+    price = models.DecimalField(decimal_places=2, max_digits=12)
     vendor = models.ForeignKey(Vendor)
 
     @property
@@ -41,8 +32,17 @@ class Product(models.Model):
         return math.ceil((self.cost * 2) + self.vendor.drop_shipping_price) - 1
 
 
-class Vendor(models.Model):
-    drop_shipping_price = models.DecimalField(decimal_places=2, max_length=6)
+class WishList(models.Model):
+    model = models.ForeignKey(Model)
+    title = models.CharField(max_length=40)
+    description = models.TextField(blank=True)
+
+
+class WishListLines(models.Model):
+    product = models.ForeignKey(Product)
+    wish_list = models.ForeignKey(WishList)
+    purchased = models.BooleanField(default=False)
+
 
 
 class Order(models.Model):
